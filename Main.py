0c94f4b01e614137f2390f56956ca015c376ebff
LYCNK <91485463+LYCNK@users.noreply.github.com>
2024-05-10 17:00:11 +0100

    Fixes and layers
    Fixed glitch where picture quality would severely decrease if window resized in a specific way. renamed the channel "voice" to "music" because it plays music. Added a picture that is layered over the main menu surface.

--- Main.py
+++ Main.py
@@ -11,6 +11,8 @@ pygame.display.set_caption("GARE") ## window name
 
 mainMenuTest = pygame.image.load("assets\\main menu template.png").convert() ## making the GUI surface
 mainMenuTest = pygame.transform.scale(mainMenuTest, (X, Y)) ## scaling the gui surface to the beginning window size
+buttonTest = pygame.image.load("assets\\button.png").convert()
+
 icon = pygame.image.load("assets\\icon.png").convert() ## making the icon surface
 
 pygame.display.set_icon(icon) ## setting the icon in the top left
@@ -18,10 +20,10 @@ scrn.blit(mainMenuTest, (0, 0))
 
 mixer.init()
 pygame.mixer.set_num_channels(8)
-voice = pygame.mixer.Channel(1)
-mainMenuTestMusic = pygame.mixer.Sound("assets\\fallout 4 menu.mp3")    ## music biz, all you need to know is that the music for the main menu lies in channel 1, in a surface (for audio?) called voice
-voice.set_volume(1)
-voice.play(mainMenuTestMusic)
+music = pygame.mixer.Channel(1)
+mainMenuTestMusic = pygame.mixer.Sound("assets\\fallout 4 menu.mp3")    ## music biz, all you need to know is that the music for the main menu lies in channel 1, in a surface (for audio?) called music
+music.set_volume(1)
+music.play(mainMenuTestMusic)
 
 status = True
 while (status):
@@ -29,18 +31,21 @@ while (status):
                                                     ## RUNTIME
     
     w, h = pygame.display.get_surface().get_size() ## making variables for the current window size
+    mainMenuTest = pygame.image.load("assets\\main menu template.png").convert()
+    buttonTest = pygame.image.load("assets\\button.png").convert()
     mainMenuTest = pygame.transform.scale(mainMenuTest, (w,h))
     scrn.blit(mainMenuTest, (0, 0))
+    scrn.blit(buttonTest, (15,15))
     pygame.display.flip()
     if w != h: ## if the x and y window size are different
         pygame.display.set_mode((h, h), pygame.RESIZABLE) ## keeping a constant 1:1 ratio
-    if voice.get_busy() == False:
+    if music.get_busy() == False:
         mixer.init()
         pygame.mixer.set_num_channels(8)
-        voice = pygame.mixer.Channel(1)
+        music = pygame.mixer.Channel(1)
         mainMenuTestMusic = pygame.mixer.Sound("assets\\fallout 4 menu.mp3")    ## makes music repeat if the audo surface isn't playing anything
-        voice.set_volume(1)
-        voice.play(mainMenuTestMusic)
+        music.set_volume(1)
+        music.play(mainMenuTestMusic)
         
                                                     ## RUNTIME
         
