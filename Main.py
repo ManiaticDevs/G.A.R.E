905dbc5f409adb357a931e66556e8dd69386cef9
LYCNK <91485463+LYCNK@users.noreply.github.com>
2024-05-23 17:55:28 +0100

    Continuity update
    I'm just uploading this so i can work on it elsewhere, I've been trying to detect mouse over surface

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import pygame
 from pygame import mixer
 
 pygame.init()
-X = 700
-Y = 700
+X = 900
+Y = 900
 
 scrn = pygame.display.set_mode((X, Y), pygame.RESIZABLE) ## window size and calling it resizable
 
@@ -21,8 +21,8 @@ scrn.blit(mainMenuTest, (0, 0))
 mixer.init()
 pygame.mixer.set_num_channels(8)
 music = pygame.mixer.Channel(1)
-mainMenuTestMusic = pygame.mixer.Sound("assets\\fallout 4 menu.mp3")    ## music biz, all you need to know is that the music for the main menu lies in channel 1, in a surface (for audio?) called music
-music.set_volume(1)
+mainMenuTestMusic = pygame.mixer.Sound("assets\\ApocalypticEchoes.mp3")    ## music biz, all you need to know is that the music for the main menu lies in channel 1, in a surface (for audio?) called music
+music.set_volume(0.1)
 music.play(mainMenuTestMusic)
 
 status = True
@@ -30,23 +30,31 @@ while (status):
     
                                                     ## RUNTIME
     
-    w, h = pygame.display.get_surface().get_size() ## making variables for the current window size
-    mainMenuTest = pygame.image.load("assets\\main menu template.png").convert()
-    buttonTest = pygame.image.load("assets\\button.png").convert()
-    mainMenuTest = pygame.transform.scale(mainMenuTest, (w,h))
+    screen_w, screen_h = pygame.display.get_surface().get_size() ## making variables for the current window size
+    button_w, button_h = buttonTest.get_size()
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    buttonTest_min_x, buttonTest_min_y, button_w, button_h = buttonTest.get_rect()
+    buttonTest_max_x, buttonTest_max_y = buttonTest_min_x + button_w, buttonTest_min_y + button_h
+    print(buttonTest_min_x, buttonTest_min_y, buttonTest_max_x, buttonTest_max_y)
+    mainMenuTest = pygame.transform.scale(mainMenuTest, (screen_w, screen_h))
     scrn.blit(mainMenuTest, (0, 0))
-    scrn.blit(buttonTest, (15,15))
+    buttonTest = pygame.transform.scale(buttonTest, (screen_w/3, screen_h/3))
+    scrn.blit(buttonTest, ((screen_w/2) - (button_w/2), (screen_h/2) - (button_h/2)))
     pygame.display.flip()
-    if w != h: ## if the x and y window size are different
-        pygame.display.set_mode((h, h), pygame.RESIZABLE) ## keeping a constant 1:1 ratio
+    if screen_w != screen_h: ## if the x and y window size are different
+        pygame.display.set_mode((screen_h, screen_h), pygame.RESIZABLE) ## keeping a constant 1:1 ratio
+        mainMenuTest = pygame.image.load("assets\\main menu template.png").convert()
+        buttonTest = pygame.image.load("assets\\button.png").convert()
     if music.get_busy() == False:
         mixer.init()
         pygame.mixer.set_num_channels(8)
         music = pygame.mixer.Channel(1)
-        mainMenuTestMusic = pygame.mixer.Sound("assets\\fallout 4 menu.mp3")    ## makes music repeat if the audo surface isn't playing anything
-        music.set_volume(1)
+        mainMenuTestMusic = pygame.mixer.Sound("assets\\ApocalypticEchoes.mp3")    ## makes music repeat if the audo surface isn't playing anything
+        music.set_volume(0.1)
         music.play(mainMenuTestMusic)
-        
+    if (buttonTest_min_x<=mouse_x and mouse_x<=buttonTest_max_x) and (buttonTest_min_y<=mouse_y and mouse_y<=buttonTest_max_y):
+        print("high")
+    
                                                     ## RUNTIME
         
     for i in pygame.event.get():
